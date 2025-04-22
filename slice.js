f86dccc17d1b708b0c55a04cb2d30d2a18077e73
ram <bvn.ramkumar@gmail.com>
2025-04-22 17:14:49 +0530

    slice method practiced

---
+++ slice.js
@@ -0,0 +1,7 @@
+let sliceArr = ["birds","trees","fish","land","sea","boats"];
+let sliceArr1 = sliceArr.slice(-1);
+//console.log(sliceArr1);
+
+let sliceArr2 = ["fire", "water", "ice", "steam", "sand", "grass"];
+let sliceArr3 = sliceArr2.slice(-5,-2);  
+console.log(sliceArr3);
